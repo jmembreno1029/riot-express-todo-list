acc562c7e542088e74cb77e8d5a2fdf6bccd36db
Joshua Membreno <membrenojoshua@gmail.com>
2018-10-29 12:37:29 -0500

    update

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,6 @@
-language: node_js
-node_js:
-  - "8"
-services: mongodb
-cache:
-  directories:
-    - "node_modules"
-sudo: false
-install:
-  - npm install
-script:
-  - npm test
-deploy:
-  - provider: script
-    skip_cleanup: true
-    script: chmod +x ./deploy.sh && ./deploy.sh
-    on:
-      branch: master
+echo "Installing 'node-cmd'"
+npm install node-cmd
+echo "Installing 'node-ssh'"
+npm install node-ssh
+echo "Running 'deploy.js'"
+node deploy.js
